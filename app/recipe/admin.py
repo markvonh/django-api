fa88ce9b520be2aa5d36b0cba93a9aebe425da8a
markvonh <markvonh@gmail.com>
2020-12-26 21:38:31 +0100

    Refactor views, use super class added ingredient

--- app/recipe/admin.py
+++ app/recipe/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Tag
+from .models import Tag, Ingredient
 
 
 admin.site.register(Tag)
+admin.site.register(Ingredient)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,25 +2,38 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from .models import Tag
-from .serializers import TagSerializer
+from .models import Tag, Ingredient
+from .serializers import TagSerializer, IngredientSerializer
 
 
-class TagViewSet(
+class BaseRecipeAttrViewSet(
     viewsets.GenericViewSet,
     mixins.ListModelMixin,
     mixins.CreateModelMixin,
 ):
-    """Manage tag in the database"""
+    """Base viewset for user owned recipe attributes"""
 
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
-    queryset = Tag.objects.all()
-    serializer_class = TagSerializer
 
     def get_queryset(self):
+        """Return objects for the current authenticated user only"""
         return self.queryset.filter(user=self.request.user).order_by("-name")
 
     def perform_create(self, serializer):
-        """Create a new Tag"""
+        """Create a new ingredient"""
         serializer.save(user=self.request.user)
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database"""
+
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
+
+
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database"""
+
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
